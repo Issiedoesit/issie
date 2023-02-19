ae26bb3cb8cb1b6afaf451539e409237c1bfcce0
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-02-19 15:47:21 +0100

    testing folder structure with window href public folder

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,27 +4,27 @@ const ProjectsData = [
         name: "MLC",
         description: "A real estate project.",
         link: "https://minimum-living-cost.vercel.app/",
-        image: "public/assets/media/images/projects/MLC.png",
+        image: "/assets/media/images/projects/MLC.png",
         techStack: [
             {
                 id: 1,
                 name: "React",
-                icon:"public/assets/media/icons/react.svg"
+                icon:"/assets/media/icons/react.svg"
             },
             {
                 id: 2,
                 name: "Jquery",
-                icon:"public/assets/media/icons/jquery.svg"
+                icon:"/assets/media/icons/jquery.svg"
             },
             {
                 id: 3,
                 name: "Tailwind",
-                icon:"public/assets/media/icons/tailwind.svg"
+                icon:"/assets/media/icons/tailwind.svg"
             },
             {
                 id: 2,
                 name: "Javascript",
-                icon:"public/assets/media/icons/js.svg"
+                icon:"/assets/media/icons/js.svg"
             }
         ]
     }
