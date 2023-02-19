c96f0a2104388f1b97fb7c7fd6aca6f2be77b7fa
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-02-19 15:42:35 +0100

    testing folder structure with window href remove slach

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,27 +4,27 @@ const ProjectsData = [
         name: "MLC",
         description: "A real estate project.",
         link: "https://minimum-living-cost.vercel.app/",
-        image: "/src/assets/media/images/projects/MLC.png",
+        image: "src/assets/media/images/projects/MLC.png",
         techStack: [
             {
                 id: 1,
                 name: "React",
-                icon:"/src/assets/media/icons/react.svg"
+                icon:"src/assets/media/icons/react.svg"
             },
             {
                 id: 2,
                 name: "Jquery",
-                icon:"/src/assets/media/icons/jquery.svg"
+                icon:"src/assets/media/icons/jquery.svg"
             },
             {
                 id: 3,
                 name: "Tailwind",
-                icon:"/src/assets/media/icons/tailwind.svg"
+                icon:"src/assets/media/icons/tailwind.svg"
             },
             {
                 id: 2,
                 name: "Javascript",
-                icon:"/src/assets/media/icons/js.svg"
+                icon:"src/assets/media/icons/js.svg"
             }
         ]
     }
