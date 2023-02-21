01aa316a2d49dc5db3b2229eeb74372f141ec36b
Isioma Ekwemuka <105283590+Issiedoesit@users.noreply.github.com>
2023-02-21 12:18:48 +0100

    Add wordive

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -27,6 +27,35 @@ const ProjectsData = [
                 icon:"/assets/media/icons/js.svg"
             }
         ]
+    },
+        {
+        id: 2,
+        name: "Wordive",
+        description: "A dictionary app.",
+        link: "https://wordive.vercel.app/",
+        image: "/assets/media/images/projects/MLC.png",
+        techStack: [
+            {
+                id: 1,
+                name: "React",
+                icon:"/assets/media/icons/react.svg"
+            },
+            {
+                id: 2,
+                name: "Jquery",
+                icon:"/assets/media/icons/jquery.svg"
+            },
+            {
+                id: 3,
+                name: "Tailwind",
+                icon:"/assets/media/icons/tailwind.svg"
+            },
+            {
+                id: 2,
+                name: "Javascript",
+                icon:"/assets/media/icons/js.svg"
+            }
+        ]
     }
 ]
 
