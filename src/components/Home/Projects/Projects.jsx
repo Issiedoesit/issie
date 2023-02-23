9d052627fa4ae97d4b0bc0678ff6e19141f3cadc
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-02-23 23:46:47 +0100

    add pandascrow and shoppersbag

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionHeader from '../../../reusables/Text/Headers/SectionHeader'
 import SectionWrap from '../../../reusables/Wraps/SectionWrap'
 import ProjectsData from '../../../data/ProjectsData'
@@ -9,11 +9,23 @@ const Projects = () => {
 
     const location = window.location.href;
 
+    const [filter, setFilter] = useState('')
+
+    const filteredData = ProjectsData.filter((item) => filter === '' || item.type == filter)
+
   return (
     <SectionWrap>
         <SectionHeader highLightBorderColor={'border-b-brandOrange1x'} remText={'Projects'} textSpan={'Portfolio'} />
-        <div className='py-20 w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-8 lg:gap-12 text-theme-text-color'>
-           {ProjectsData.map((data, index)=>{
+        <div className='flex flex-row items-center gap-5 sm:gap-10 justify-between pt-10 pb-5 text-xs sm:text-sm md:text-base'>
+          <div>Filter:</div>
+          <div className='flex flex-row items-center text-xs sm:text-sm md:text-base gap-1 sm:gap-4  text-theme-text-color'>
+            <button type='button' onClick={()=>setFilter('')} className={`filter-project transition-all ease-in-out duration-500 ${filter === '' ? 'bg-brandOrange1x' : 'hover:bg-brandOrange1x/50 bg-transparent'} rounded-fifty px-6 py-2`}>All</button>
+            <button type='button' onClick={()=>setFilter('professional')} className={`filter-project transition-all ease-in-out duration-500 ${filter === 'professional' ? 'bg-brandOrange1x' : 'hover:bg-brandOrange1x/50 bg-transparent'} rounded-fifty px-6 py-2`}>Professional</button>
+            <button type='button' onClick={()=>setFilter('personal')} className={`filter-project transition-all ease-in-out duration-500 ${filter === 'personal' ? 'bg-brandOrange1x' : 'hover:bg-brandOrange1x/50 bg-transparent'} rounded-fifty px-6 py-2`}>Personal</button>
+          </div>
+        </div>
+        <div className='py-20 w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12 text-theme-text-color'>
+           {filteredData.map((data, index)=>{
                return <ProjectCards data={data[index]} id={'project'+data.id} key={index} projectImg={`${data.image}`} projectName={data.name} link={data.link} description={data.description} stacks={data.techStack} />
            })}
         </div>
--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,6 +4,7 @@ const ProjectsData = [
         name: "MLC",
         description: "A real estate project.",
         link: "https://minimum-living-cost.vercel.app/",
+        type:'personal',
         image: "/assets/media/images/projects/MLC.png",
         techStack: [
             {
@@ -28,13 +29,14 @@ const ProjectsData = [
             }
         ]
     },
-        {
-        id: 2,
-        name: "Wordive",
-        description: "A dictionary app.",
-        link: "https://wordive.vercel.app/",
-        image: "https://res.cloudinary.com/issie/image/upload/v1676978843/Portfolio/Wordive.jpg",
-        techStack: [
+    {
+    id: 2,
+    name: "Wordive",
+    description: "A dictionary app.",
+    link: "https://wordive.vercel.app/",
+    type:'personal',
+    image: "https://res.cloudinary.com/issie/image/upload/v1676978843/Portfolio/Wordive.jpg",
+    techStack: [
             {
                 id: 1,
                 name: "React",
@@ -52,6 +54,56 @@ const ProjectsData = [
             },
             {
                 id: 2,
+                name: "Javascript",
+                icon:"/assets/media/icons/js.svg"
+            }
+        ]
+    },
+    {
+    id: 3,
+    name: "Pandascrow Dashboard",
+    description: "Dashboard for Pandascrow users to view and manage transactions.",
+    link: "https://app.pandascrow.io/",
+    type:'professional',
+    image: "https://res.cloudinary.com/issie/image/upload/v1677192078/Portfolio/pandascrow_adem1e.png",
+    techStack: [
+            {
+                id: 1,
+                name: "Jquery",
+                icon:"/assets/media/icons/jquery.svg"
+            },
+            {
+                id: 2,
+                name: "Tailwind",
+                icon:"/assets/media/icons/tailwind.svg"
+            },
+            {
+                id: 3,
+                name: "Javascript",
+                icon:"/assets/media/icons/js.svg"
+            }
+        ]
+    },
+    {
+    id: 4,
+    name: "ShoppersBag",
+    description: "An Eco-friendly shopping bag website.",
+    link: "https://shoppersbag.co/",
+    type:'professional',
+    image: "https://res.cloudinary.com/issie/image/upload/v1677191110/Portfolio/shoppersbag_wjkkm5.png",
+    techStack: [
+            {
+                id: 1,
+                name: "Jquery",
+                icon:"/assets/media/icons/jquery.svg"
+            },
+            {
+                id: 2,
+                name: "Tailwind",
+                icon:"/assets/media/icons/tailwind.svg"
+            },
+            {
+                id: 3,
                 name: "Javascript",
                 icon:"/assets/media/icons/js.svg"
             }
